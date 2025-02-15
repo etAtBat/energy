c83cf6a19c7d40bb9f162fd4ce996b813878787e
Egan Tamashiro <egantamashiro@gmail.com>
2025-02-15 10:33:08 -0800

    feature: display history results after successful request

--- src/components/EnergyHistoryForm.tsx
+++ src/components/EnergyHistoryForm.tsx
@@ -6,62 +6,106 @@ export const EnergyHistoryForm = () => {
   const auth = useAuth();
   const [datepickerStartValue, updateDatepickerStartValue] = useState('');
   const [datepickerEndValue, updateDatepickerEndValue] = useState('');
+  const [userHistoricalItems, updateUserHistoricalItems] = useState([]) as Array<any>;
   const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
-      await fetch('/api/alerts', {
-        method: "POST",
-        body: JSON.stringify({
-          startDate: datepickerStartValue,
-          endDate: datepickerEndValue,
-          accessToken: auth.user?.access_token,
-          userId: auth?.user?.profile?.sub
-        }),
+      const response = await fetch(`/api/energy/history?startDate=${datepickerStartValue}&endDate=${datepickerEndValue}&accessToken=${auth.user?.access_token}&userId=${auth?.user?.profile?.sub}`, {
+        method: "GET",
         headers: {
           "Content-Type": "application/json",
         }
       });
-      alert("Success");
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      // console.log('Data fetched:', data);
+      const userItems = data?.items?.data;
+      console.log('userItems');
+      console.log(userItems);
+      if (Array.isArray(userItems)) {
+        updateUserHistoricalItems(userItems);
+      }
+      // return data;
     } catch (error) {
       console.error(error);
     }
   }
 
   return (
-    <form id="manual-history-form" onSubmit={onSubmit}>
-      <div>
-        <label htmlFor="start">Retreive energy usage start date</label>
-          <input
-            type="date"
-            id="start"
-            value={datepickerStartValue}
-            min="2020-01-01"
-            max="2026-12-31"
-            onChange={(event) => {
-              const selectedDate = event?.target?.value;
-              if (selectedDate) {
-                updateDatepickerStartValue(selectedDate);
-              }
-            }}
-          />
-        </div>
+    <>
+      <form id="manual-history-form" onSubmit={onSubmit}>
         <div>
-        <label htmlFor="start">Retreive energy usage end date</label>
-          <input
-            type="date"
-            id="end"
-            value={datepickerEndValue}
-            min="2020-01-01"
-            max="2026-12-31"
-            onChange={(event) => {
-              const selectedDate = event?.target?.value;
-              if (selectedDate) {
-                updateDatepickerEndValue(selectedDate);
-              }
-            }}
-          />
-        </div>
-      <button type="submit">Submit</button>
-    </form>
+          <label htmlFor="start">Retreive energy usage start date</label>
+            <input
+              type="date"
+              id="start"
+              value={datepickerStartValue}
+              min="2020-01-01"
+              max="2026-12-31"
+              onChange={(event) => {
+                const selectedDate = event?.target?.value;
+                if (selectedDate) {
+                  updateDatepickerStartValue(selectedDate);
+                }
+              }}
+            />
+          </div>
+          <div>
+          <label htmlFor="start">Retreive energy usage end date</label>
+            <input
+              type="date"
+              id="end"
+              value={datepickerEndValue}
+              min="2020-01-01"
+              max="2026-12-31"
+              onChange={(event) => {
+                const selectedDate = event?.target?.value;
+                if (selectedDate) {
+                  updateDatepickerEndValue(selectedDate);
+                }
+              }}
+            />
+          </div>
+        <button type="submit">Submit</button>
+      </form>
+      {
+        userHistoricalItems?.length ?
+           (
+            <table id="historical-table">
+              <thead>
+                <tr>
+                  <th>User ID</th>
+                  <th>Date</th>
+                  <th>Usage</th>
+                  <th>Threshold</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  userHistoricalItems.map((item: any) => {
+                    const {
+                      threshold,
+                      date,
+                      usage,
+                      userId,
+                    } = item;
+                    return (
+                      <tr key={`${userId}-${date}`}>
+                        <td>{userId}</td>
+                        <td>{date}</td>
+                        <td>{usage}</td>
+                        <td>{threshold}</td>
+                      </tr>
+                    )
+                  })
+                }
+              </tbody>
+            </table>
+          )
+          : null
+      }
+    </>
   );
 }
--- src/pages/api/energy/history.ts
+++ src/pages/api/energy/history.ts
@@ -2,16 +2,54 @@ import type { NextApiRequest, NextApiResponse } from 'next'
  
 type ResponseData = {
   message: string;
-  startDate?: string;
-  endDate?: string;
+  items?: Array<any>;
 }
  
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
+  const { method } = req;
+  console.log('method');
+  console.log(method);
+  if (method === 'GET') {
+    console.log('yep we in the get')
+    const startDate = (req.query?.startDate || '') as string;
+    const endDate = (req.query?.endDate || '') as string;
+    const accessToken = (req.query?.accessToken || '') as string;
+    const userId = (req.query?.userId || '') as string;
+    console.log('accessToken');
+    console.log(accessToken);
 
-  const startDate = (req.query?.startDate || '') as string;
-  const endDate = (req.query?.endDate || '') as string;
-  res.status(200).json({ message: 'History from Next.js!', startDate, endDate });
+    const url = `${process.env.API_GATEWAY_INVOKE_URL}/energy/history/?userId=${userId}&startDate=${startDate}&endDate=${endDate}`;
+    // const data = {
+    //   date,
+    //   userId,
+    //   usage,
+    // };
+
+    try {
+      const result = await fetch(url, {
+        method: "GET",
+        // body: JSON.stringify(data),
+        headers: {
+          "Authorization": accessToken
+        },
+      }) as any;
+      // console.log('result');
+      // console.log(result);
+      const fetchResult = await result.json();
+      const userEnergyItems = fetchResult?.data || [];
+      console.log('fetchResult');
+      console.log(fetchResult);
+      console.log('userEnergyItems');
+      console.log(userEnergyItems);
+      return res.status(200).json({ message: "Energy history fetched successfully", items: fetchResult });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: "Something went wrong" });
+    }
+  } else {
+    res.status(200).json({ message: 'History from Next.js!'});
+  }
 }
