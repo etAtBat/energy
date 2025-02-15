b27d6b4fc74d5528681e5718401a494ccf87b07f
Egan Tamashiro <egantamashiro@gmail.com>
2025-02-15 11:16:44 -0800

    chore: add basic spacing and styles to sign in / sign out buttons

--- src/components/CognitoLogin.tsx
+++ src/components/CognitoLogin.tsx
@@ -24,18 +24,14 @@ export const CognitoLogin = (): React.ReactElement => {
     return (
       <div>
         <pre> Hello: {auth.user?.profile.email} </pre>
-        <pre> ID Token: {auth.user?.id_token} </pre>
-        <pre> Access Token: {auth.user?.access_token} </pre>
-        <pre> Refresh Token: {auth.user?.refresh_token} </pre>
-
-        <button onClick={() => auth.removeUser()}>Sign out</button>
+        <button id="sign-out" onClick={() => auth.removeUser()}>Sign out</button>
       </div>
     );
   }
 
   return (
     <div>
-      <button onClick={() => auth.signinRedirect()}>Sign in</button>
+      <button id="sign-in" onClick={() => auth.signinRedirect()}>Sign in</button>
       {auth?.user && <button onClick={() => signOutRedirect()}>Sign out</button>}
     </div>
   );
