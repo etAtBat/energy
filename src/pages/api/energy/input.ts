38bc218d6ae21ff3047646ecc20f6c002362cf6e
Egan Tamashiro <egantamashiro@gmail.com>
2025-02-14 18:21:49 -0800

    chore: cleanup /energy/input api route

--- src/pages/api/energy/input.ts
+++ src/pages/api/energy/input.ts
@@ -1,5 +1,4 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import axios from "axios";
  
 type ResponseData = {
   message: string
@@ -12,16 +11,6 @@ export default async function handler(
   const { method } = req;
   if (method === "POST") {
     const { date, usage, accessToken, userId } = req.body;
-    // console.log("date");
-    // console.log(date);
-    // console.log("usage");
-    // console.log(usage);
-    // console.log("accessToken");
-    // console.log(accessToken);
-    // console.log("fetch");
-    // console.log(fetch);
-    // console.log("userId");
-    // console.log(userId);
 
     const url = `${process.env.API_GATEWAY_INVOKE_URL}/energy/input`;
     const data = {
@@ -30,60 +19,20 @@ export default async function handler(
       usage,
     };
 
-    console.log('accessToken');
-    console.log(accessToken);
-    console.log("url");
-    console.log(url);
-    console.log("data");
-    console.log(data);
     try {
-      console.log("Authorization.....")
-      // const result = await fetch(url, {
-      //   method: "POST",
-      //   body: JSON.stringify(data),
-      //   headers: {
-      //     // "Authentication": accessToken
-      //     "Authentication": 'sfdsljfklsdl-this-wont-work'
-      //   },
-      // });
-      console.log(`${accessToken}`);
-      console.log(`${accessToken}`);
-      const result = axios({
+      await fetch(url, {
         method: "POST",
-        url,
+        body: JSON.stringify(data),
         headers: {
-          "Authorization": `${accessToken}`,
-          "Content-Type": "application/json",
-          "Cache-Control": "no-cache",
-          "Accept": "*/*",
-          "Accept-Encoding": "gzip, deflate, br",
+          "Authorization": accessToken
         },
-        data: JSON.stringify(data),
-      })
-      .then(function (response) {
-        // handle success
-        console.log('response success');
-        console.log(response);
-        return res.status(200).json({ message: "Energy data saved successfully" });
-      })
-      .catch(function (error) {
-        // handle error
-        console.log('response error');
-        // console.log(error.request);
-        return res.status(500).json({ message: "something went awry" });
-
       });
+      return res.status(200).json({ message: "Hello from energy input!" });
     } catch (error) {
       console.error(error);
       return res.status(500).json({ message: "Something went wrong" });
     }
   } else {
-    return res.status(202).json({ message: "Hello from energy input!" });
+    return res.status(200).json({ message: "Hello from energy input!" });
   }
 }
-
-export const config = {
-  api: {
-    externalResolver: true,
-  },
-};
