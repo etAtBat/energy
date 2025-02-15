2859c420d79aede6e8aebfb4fca819dd279adfa2
Egan Tamashiro <egantamashiro@gmail.com>
2025-02-15 13:11:26 -0800

    chore: cleanup

--- src/components/CognitoLogin.tsx
+++ src/components/CognitoLogin.tsx
@@ -24,6 +24,9 @@ export const CognitoLogin = (): React.ReactElement => {
     return (
       <div>
         <pre> Hello: {auth.user?.profile.email} </pre>
+        {/* <pre> ID Token: {auth.user?.id_token} </pre>
+        <pre> Access Token: {auth.user?.access_token} </pre>
+        <pre> Refresh Token: {auth.user?.refresh_token} </pre> */}
         <button id="sign-out" onClick={() => auth.removeUser()}>Sign out</button>
       </div>
     );
